4b0d2708ff0dc4ce07eb83ec7fbe6c28c56c1060
Jazeps Ivulis <jazeps.ivulis@accenture.com>
2023-08-21 19:27:41 +0300

    Initial commit

---
+++ TemperatureConvertor/TemperatureConvertorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConvertorApp.swift
+//  TemperatureConvertor
+//
+//  Created by jazeps.ivulis on 21/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConvertorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
